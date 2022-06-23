8b11acf6a7d3f5f2c152129f3af1193996501908
akshita-singh-github <akshitasinghdei123@gmail.com>
2022-06-23 09:58:33 +0530

    first commit

---
+++ restaurant/Controllers/RestaurantController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restaurant.Data;
+
+namespace restaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public RestaurantController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+         
+        public async Task<ActionResult<List<RestoList>>> GetRestoList()
+        {
+            return Ok(await _context.Listresto.ToListAsync());
+        }
+
+      [HttpPost]
+        public async Task<ActionResult<List<RestoList>>> CreateRestoList(RestoList details)
+        {
+             _context.Listresto.Add(details);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Listresto.ToListAsync());
+
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<List<RestoList>>> UpdateRestoList(RestoList details)
+        {
+            var dbresto= await _context.Listresto.FindAsync(details.Id);
+            if (dbresto == null)
+                return BadRequest("not found");
+
+            dbresto.Name = details.Name;
+            dbresto.Email = details.Email;
+            dbresto.Address = details.Address;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Listresto.ToListAsync());
+
+        }
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<List<RestoList>>> DeleteRestoList(int id)
+        {
+            var dbresto = await _context.Listresto.FindAsync(id);
+            if (dbresto == null)
+                return BadRequest("not found");
+
+            _context.Listresto.Remove(dbresto);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Listresto.ToListAsync());
+        }
+
+    }
+}
---
+++ restaurant/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace restaurant.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+        public DbSet<RestoList> Listresto => Set<RestoList>() ;
+    }
+}
